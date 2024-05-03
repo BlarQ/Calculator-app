478968e280fa0e4c65d3d1e1e162e0b3f318c530
BlarQ <collinsogunlala@gmail.com>
2024-05-03 22:53:25 +0100

    fix: background color and text color
    I was able to fix the background color on different modes and also make the text switch colors on diferent modes. Noow its remaining the animation of the toggle button to right per click

--- script.js
+++ script.js
@@ -3,42 +3,58 @@ const button = document.getElementById('buttonEqual')
 const buttonRes = document.getElementById('buttonRes')
 const buttonDel = document.getElementById('buttonDel')
 const bgToggler = document.getElementById('bgToggler')
+const textColor = document.getElementById('head')
+const textColor1 = document.getElementById('head1')
+const textColor2 = document.getElementById('head2')
+const textColor3 = document.getElementById('head3')
+const textColor4 = document.getElementById('head4')
 
 
+//Delete Function
 const deleteButton =() => {
     input.value = input.value.slice(0, -1)
 }
 
 buttonDel.addEventListener('click', deleteButton)
 
+//Reset Function
 const resetButton = () => {
     input.value = ''
 }
 
 buttonRes.addEventListener('click', resetButton)
 
+//Compute Function(Equal to)
 const computeResult = () => {
-    const result = eval(input.value)
-    input.value = result
+    try{
+        const result = eval(input.value)
+        input.value = result
+    }
+    catch(err){
+        input.value = 'Error'
+    }
 }
 
 button.addEventListener('click', computeResult)
 
-// const bgColor = () => {
-//     document.body.style.backgroundColor ='cyan'
-//     document.body.style.backgroundColor ='green'
-// }
-
-// bgToggler.addEventListener('click', bgColor)
-
+//Background Color Functions
 const colors = ['white', '#3e4d80', '#1d2951']
+const tColor = ['black', 'white', 'cyan']
 
 colorIndex = 0
+tColorIndex = 0
 
 const bgColor = () => {
     document.body.style.backgroundColor = colors[colorIndex]
     colorIndex++
     colorIndex === colors.length ? colorIndex = 0 : colorIndex
+    textColor.style.color = tColor[tColorIndex]
+    textColor1.style.color = tColor[tColorIndex]
+    textColor2.style.color = tColor[tColorIndex]
+    textColor3.style.color = tColor[tColorIndex]
+    textColor4.style.color = tColor[tColorIndex]
+    tColorIndex++
+    tColorIndex === tColor.length ? tColorIndex = 0 : tColorIndex
 }
 
 bgToggler.addEventListener('click', bgColor)
